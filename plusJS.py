c5b389a97a333b09213f4b2d81d49b6e27bb7c1b
Nathan <nathannecro+olinnathan@gmail.com>
2012-11-13 15:03:17 -0500

    tkinter test (yes, I know I spelled wrong)

--- plusJS.py
+++ plusJS.py
@@ -2,7 +2,7 @@ __author__ = 'Nathan'
 
 def tanglekit(imput):
     body = imput
-    head = "<link rel="stylesheet" href="TangleKit/TangleKit.css" type="text/css">
+    head = "<script type="text/javascript" src="Tangle.js"></script><link rel="stylesheet" href="TangleKit/TangleKit.css" type="text/css">
     <script type="text/javascript" src="TangleKit/mootools.js"></script>
     <script type="text/javascript" src="TangleKit/sprintf.js"></script>
     <script type="text/javascript" src="TangleKit/BVTouchable.js"></script>
