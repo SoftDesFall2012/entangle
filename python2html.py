76ea55f2fe3f7d2f2f3c3f4bbfc235165fd38b17
Jong Hewk Park <jonghewk715@gmail.com>
2012-11-14 13:56:19 -0500

    searching \n

--- python2html.py
+++ python2html.py
@@ -1,16 +1,22 @@
 __author__ = 'jpark3'
 
 """
- python2html function: Simply converts the string of the python_output (text editor) to a HTML format.
-                       It assumes that the python_output is formatted.
-                       Ex) If a user press 'enter', the python script should have inserted '\n' to the string
+ python2html function: Simply converts the string of the python_output (text editor made in Tkinter) to a HTML format.
+
 """
 
 def python2html(pyout):
 
+    for a in pyout:
+        if a == '\':
+            print 'hello'
+
+
     start_tag= '<html>\n\t<body>\n\t\t<p>'
     end_tag='</p>\n\t</body>\n</html>'
+
     html= start_tag + pyout + end_tag
+
 
     fout=open('output.html','w')
     fout.write(html)
@@ -20,7 +26,7 @@ def python2html(pyout):
 
 # Example code for python2html function
 def example():
-    in1='When you eat 3 cookies [parent variable, class identifier, param_start, param_end, function]' \
+    in1='When you eat 3 cookies [parent variable, class identifier, param_start, param_end, function]\n' \
         ' you consume 150 [child variable,  class identifier] calories.'
 
     print python2html(in1)
