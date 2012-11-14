6ff82fb0ad0a18b031ac7e699425566897d6ffa6
Nathan <nathannecro+olinnathan@gmail.com>
2012-11-14 13:27:34 -0500

    Input + HTMLParsing

--- plusJS.py
+++ plusJS.py
@@ -1,12 +1,34 @@
 __author__ = 'Nathan'
 
-def tanglekit(imput):
-    body = imput
-    head = "<script type="text/javascript" src="Tangle.js"></script><link rel="stylesheet" href="TangleKit/TangleKit.css" type="text/css">
-    <script type="text/javascript" src="TangleKit/mootools.js"></script>
-    <script type="text/javascript" src="TangleKit/sprintf.js"></script>
-    <script type="text/javascript" src="TangleKit/BVTouchable.js"></script>
-    <script type="text/javascript" src="TangleKit/TangleKit.js"></script>"
-    append = head + body
-    return append
+from HTMLParser import *
 
+python2html_output = open('output.html')
+
+print(python2html_output)
+
+class HTMLParse(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print "Start tag:", tag
+    def handle_endtag(self, tag):
+        print "End tag :", tag
+    def handle_data(self, data):
+        print "Encountered some data  :", data
+
+
+parse  = HTMLParse()
+parse.feed(python2html_output.read())
+
+'''
+class jsInjection(filename)
+
+    def tanglekit(imput):
+        body = imput
+        head = "<script type="text/javascript" src="Tangle.js"></script><link rel="stylesheet" href="TangleKit/TangleKit.css" type="text/css">
+        <script type="text/javascript" src="TangleKit/mootools.js"></script>
+        <script type="text/javascript" src="TangleKit/sprintf.js"></script>
+        <script type="text/javascript" src="TangleKit/BVTouchable.js"></script>
+        <script type="text/javascript" src="TangleKit/TangleKit.js"></script>"
+        append = head + body
+        return append
+
+'''
