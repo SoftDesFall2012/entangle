fb589b75d26268459586fd54e59061eeec0cc1cb
Jong Hewk Park <jonghewk715@gmail.com>
2012-11-15 22:27:41 -0500

    adding comments to python2html

--- python2html.py
+++ python2html.py
@@ -1,33 +1,23 @@
 __author__ = 'jpark3'
 
-"""
- python2html function: Simply converts the string of the python_output (text editor made in Tkinter) to a HTML format.
 
-"""
+# python2html function: Simply converts the string of the python_output (text editor made in Tkinter) to a HTML format.
 
 def python2html(pyout):
-    '''
-        for a in pyout:
-            if a == '\':
-                print 'hello'
 
+    output = pyout.replace("\n", "<br \>")  # add a break to a new line
 
-        start_tag= '<html>\n\t<body>\n\t\t<p>'
-        end_tag='</p>\n\t</body>\n</html>'
+    start_tag= '<body>\n\t\t<p>' # start html body
+    end_tag='</p>\n</body>' #end html body
 
-        html= start_tag + pyout + end_tag
-    '''
+    html= start_tag + output + end_tag # combine everything in string
 
-    output = pyout.replace("\n", "<br \>")
-    return output
-
-'''
-    fout=open('output.html','w')
+    fout=open('output.html','w') #save it in a html format
     fout.write(html)
     fout.close()
 
     return html
-'''
+
 # Example code for python2html function
 def example():
     in1='When you eat 3 cookies [parent variable, class identifier, param_start, param_end, function]\n' \
