02614aacf9bba575faf84a822336e98423ef6291
Nathan <nathannecro+olinnathan@gmail.com>
2012-12-03 23:37:38 -0500

    cleaned deprecated comments in plusJS

--- plusJS.py
+++ plusJS.py
@@ -78,39 +78,3 @@ fout.close()
 
 print output
 
-
-'''
-
-
-#print python2html_outpututf8.format('<span data-var="cookies" class = "TKAdjustableNumber" data-min="1" data-max = "100">')
-
-#print python2html_outpututf8
-
-
-
-class HTMLParse(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        print "Start tag:", tag
-    def handle_endtag(self, tag):
-        print "End tag:", tag
-    def handle_data(self, data):
-        print "Data:", data
-
-
-parse  = HTMLParse()
-#parse.feed(python2html_output.read())
-parse.handle_data(python2html_output.read())
-
-
-class jsInjection(filename)
-
-    def tanglekit(imput):
-        body = imput
-        head = "<script type="text/javascript" src="Tangle.js"></script><link rel="stylesheet" href="TangleKit/TangleKit.css" type="text/css">
-        <script type="text/javascript" src="TangleKit/mootools.js"></script>
-        <script type="text/javascript" src="TangleKit/sprintf.js"></script>
-        <script type="text/javascript" src="TangleKit/BVTouchable.js"></script>
-        <script type="text/javascript" src="TangleKit/TangleKit.js"></script>"
-        append = head + body
-        return append
-'''
